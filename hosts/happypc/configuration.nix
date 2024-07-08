685f1b55d6a1060c037d332ab944c2b645aba762
happyori <orkhan.tahirov@gmail.com>
2024-07-08 08:54:44 -0700

    Revert "NixOSv2 generation -[29]- 2024-07-07T20:48:19Z"
    This reverts commit a3a33e2699fc8f76bcd6a95af6c946b830520a69.

--- hosts/happypc/configuration.nix
+++ hosts/happypc/configuration.nix
@@ -168,7 +168,7 @@
     powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.legacy_535;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
   nixpkgs.config.allowUnfree = true;
