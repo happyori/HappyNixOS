bd5b652ead7d126afd01fd0777ee0988a6481d41
happyori <orkhan.tahirov@gmail.com>
2024-09-08 00:44:55 -0700

    NixOSv2 generation -[49]- 2024-09-08T07:32:38Z

--- modules/system/steam.nix
+++ modules/system/steam.nix
@@ -1,17 +1,25 @@
-{ lib, config, ... }:
+{ lib, config, pkgs, ... }:
 {
   options.custom.games = {
     steam.enable = lib.mkEnableOption "Enable to install steam";
     minecraft.enable = lib.mkEnableOption "Enable to install java for minecraft";
   };
   config = {
-    programs.steam = {
-      inherit (config.custom.games.steam) enable;
-      remotePlay.openFirewall = true;
-      dedicatedServer.openFirewall = true;
-      localNetworkGameTransfers.openFirewall = true;
+    programs = {
+      steam = {
+        inherit (config.custom.games.steam) enable;
+        remotePlay.openFirewall = true;
+        dedicatedServer.openFirewall = true;
+        gamescopeSession.enable = config.custom.games.steam.enable;
+        localNetworkGameTransfers.openFirewall = true;
+      };
+      gamescope = {
+        inherit (config.custom.games.steam) enable;
+        capSysNice = true;
+      };
+      java.enable = config.custom.games.minecraft.enable;
+      gamemode.enable = config.custom.games.steam.enable;
     };
-
-    programs.java.enable = config.custom.games.minecraft.enable;
+    environment.systemPackages = lib.optional config.custom.games.steam.enable pkgs.mangohud;
   };
 }
