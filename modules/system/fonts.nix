12fc692e848ff04b089e11693f6331581fac225c
happyori <orkhan.tahirov@gmail.com>
2024-09-08 11:42:55 -0700

    NixOSv2 generation -[56]- 2024-09-08T18:28:45Z

--- modules/system/fonts.nix
+++ modules/system/fonts.nix
@@ -1,8 +1,13 @@
 { pkgs, ... }:
 {
-  fonts.packages = [
-    (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" "CascadiaCode" "JetBrainsMono" "FiraCode" ]; })
-    pkgs.inter
-    pkgs.dina-font
-  ];
+  fonts = {
+    packages = [
+      (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" "CascadiaCode" "JetBrainsMono" "FiraCode" ]; })
+      pkgs.inter
+      pkgs.dina-font
+    ];
+    fontconfig.enable = true;
+    fontDir.enable = true;
+    enableDefaultPackages = true;
+  };
 }
