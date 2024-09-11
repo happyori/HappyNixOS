eb1bb497e381416236186da5b58d02b1ab6d8219
happyori <orkhan.tahirov@gmail.com>
2024-09-11 15:33:58 -0700

    NixOSv2 generation -[74]- 2024-09-11T06:39:30Z

---
+++ extras/configs/nvim/lua/plugins/neorg.lua
@@ -0,0 +1,13 @@
+---@module 'lazy'
+---@type LazySpec
+return {
+  {
+    'nvim-neorg/neorg',
+    ft = 'norg',
+    opts = {
+      load = {
+        ['core.defaults'] = {},
+      },
+    },
+  },
+}
