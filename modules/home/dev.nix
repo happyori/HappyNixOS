82460ebe640ddd653f4a4edecab9bd69d9583435
happyori <orkhan.tahirov@gmail.com>
2024-09-11 15:44:36 -0700

    NixOSv2 generation -[77]- 2024-09-11T22:43:51Z

--- modules/home/dev.nix
+++ modules/home/dev.nix
@@ -53,7 +53,7 @@ in
       ++ optionals cfg.lang.add-rust [ pkgs.rustc pkgs.cargo pkgs.rust-analyzer pkgs.vscode-extensions.vadimcn.vscode-lldb ]
       ++ optionals cfg.lang.add-go [ pkgs.go ]
       ++ optionals cfg.lang.add-nix [ pkgs.nixd pkgs.nixpkgs-fmt pkgs.statix ]
-      ++ optionals cfg.nvim.enable [ pkgs.nodejs pkgs.sqlite pkgs.luarocks pkgs.prettierd pkgs.lua ]
+      ++ optionals cfg.nvim.enable [ pkgs.nodejs pkgs.sqlite pkgs.luarocks pkgs.prettierd pkgs.lua5_1 ]
       ++ [ pkgs.gnumake ];
   };
 }
