e203ddadb2a2d0f9e03c10008089167a27bc9ede
happyori <orkhan.tahirov@gmail.com>
2024-09-11 16:05:07 -0700

    NixOSv2 generation -[78]- 2024-09-11T22:45:05Z

--- extras/configs/nvim/lua/plugins/neorg.lua
+++ extras/configs/nvim/lua/plugins/neorg.lua
@@ -4,9 +4,47 @@ return {
   {
     'nvim-neorg/neorg',
     ft = 'norg',
+    cmd = 'Neorg',
     opts = {
       load = {
         ['core.defaults'] = {},
+        ['core.completion'] = {},
+        ['core.concealer'] = {},
+        ['core.summary'] = {},
+        ['core.dirman'] = {
+          config = {
+            workspaces = {
+              core = '~/Neorg/Core',
+            },
+            index = 'index.norg',
+          },
+        },
+      },
+    },
+  },
+  {
+    'mrjones2014/legendary.nvim',
+    opts = {
+      autocmds = {
+        {
+          name = 'Neorg Attach Source',
+          clear = true,
+          {
+            'BufRead',
+            function()
+              local cmp = require 'cmp'
+              cmp.setup.buffer {
+                sources = {
+                  { name = '[Neorg]' },
+                  { name = 'buffer', group_index = 1 },
+                },
+              }
+            end,
+            opts = {
+              pattern = { '%.norg$' },
+            },
+          },
+        },
       },
     },
   },
