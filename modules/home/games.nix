28c849e6021ff603ddc6c97495a5fdcacb452ba1
happyori <orkhan.tahirov@gmail.com>
2024-10-16 18:55:37 -0700

    NixOSv2 generation -[108]- 2024-10-17T01:19:37Z

--- modules/home/games.nix
+++ modules/home/games.nix
@@ -1,19 +1,10 @@
 { pkgs
 , ...
 }:
-let
-  vintagestory = pkgs.vintagestory.overrideAttrs (final: previous: rec {
-    version = "1.19.8";
-    src = pkgs.fetchurl {
-      url = "https://cdn.vintagestory.at/gamefiles/stable/vs_client_linux-x64_${version}.tar.gz";
-      hash = "sha256-R6J+ACYDQpOzJZFBizsQGOexR7lMyeoZqz9TnWxfwyM=";
-    };
-  });
-in
 {
   home.packages = [
     pkgs.prismlauncher
     pkgs.lutris
-    vintagestory
+    pkgs.vintagestory
   ];
 }
