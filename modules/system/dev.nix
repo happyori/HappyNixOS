c2286f38bd2a8a2776ba06b5e27e035c802eaa51
happyori <orkhan.tahirov@gmail.com>
2025-01-08 16:53:56 -0800

    NixOSv2 generation -[141]- 2025-01-08T21:19:05Z

--- modules/system/dev.nix
+++ modules/system/dev.nix
@@ -5,4 +5,11 @@
     nix-direnv.enable = true;
     loadInNixShell = true;
   };
+
+  virtualisation.docker.rootless = {
+    enable = true;
+    setSocketVariable = true;
+  };
+
+  hardware.nvidia-container-toolkit.enable = true;
 }
