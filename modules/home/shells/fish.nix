e41f82c2e9fd7142b5ca54b7c129e65bb81e6f22
happyori <orkhan.tahirov@gmail.com>
2025-01-26 11:10:54 -0800

    NixOS generation -[158]- 2025-01-26 Sun 11:10:52 AM

--- modules/home/shells/fish.nix
+++ modules/home/shells/fish.nix
@@ -15,13 +15,13 @@
         preferAbbrs = true;
         shellAbbrs = {
           lz = "lazygit";
-          l = "ls -lah --no-time --no-permissions --no-time --no-user";
           ll = "ls -lah --absolute=on";
           lla = "ls -lah";
           lld = "duf";
           gs = "git status";
         };
         shellAliases = {
+          l = "ls -lah --no-permissions --no-time --no-user";
           ls = "eza --icons always --color always --git --smart-group --group-directories-first";
         };
       };
