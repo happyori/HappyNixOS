1fc15c898ca7c9f6425b53a68ef1ea780cfb8262
happyori <orkhan.tahirov@gmail.com>
2025-03-02 14:14:42 -0800

    NixOS generation -[170]- 2025-03-02 Sun 02:14:42 PM

--- modules/home/zellij.nix
+++ modules/home/zellij.nix
@@ -1,16 +1,19 @@
-{ pkgs, paths, ... }:
+{ pkgs, paths, config, ... }:
 let
   path = paths.custom_pkgs + "/modus-theme.nix";
   modus_der = import path { inherit pkgs; };
   theme = "${modus_der}/themes/extras/zellij/modus_vivendi.kdl";
+  inherit (config.lib.file) mkOutOfStoreSymlink;
 in
 {
   programs.zellij = {
     enable = true;
     enableFishIntegration = true;
-    settings = {
-      theme = "modus_vivendi";
-    };
+    settings = { };
+  };
+
+  xdg.configFile."zellij/config.kdl" = {
+    source = mkOutOfStoreSymlink (paths.app_configs + "zellij/config.kdl");
   };
 
   xdg.configFile."zellij/themes/modus_vivendi.kdl" = {
