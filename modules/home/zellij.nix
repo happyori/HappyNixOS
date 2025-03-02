54f446021fef1c8369199e265b226d17f194f527
happyori <orkhan.tahirov@gmail.com>
2025-03-02 13:27:23 -0800

    NixOS generation -[168]- 2025-03-02 Sun 01:27:22 PM

--- modules/home/zellij.nix
+++ modules/home/zellij.nix
@@ -1,6 +1,6 @@
 { pkgs, paths, ... }:
 let
-  path = paths.custom_pkgs + "/modus_vivendi.nix";
+  path = paths.custom_pkgs + "/modus-theme.nix";
   modus_der = import path { inherit pkgs; };
   theme = "${modus_der}/themes/extras/zellij/modus_vivendi.kdl";
 in
