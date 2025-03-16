37f451678d179ead4284a77cb705623ee39c4de5
happyori <orkhan.tahirov@gmail.com>
2025-03-16 11:20:57 -0700

    NixOS generation -[191]- 2025-03-16 Sun 11:20:55 AM

--- extras/configs/nvim/lua/plugins/neorg.lua
+++ extras/configs/nvim/lua/plugins/neorg.lua
@@ -14,6 +14,7 @@ return {
           keymaps = {
             { '<leader>nc', '<cmd>Neorg workspace core<cr>', description = '|Open [C]ore Workspace' },
             { '<leader>nj', '<cmd>Neorg journal<cr>', description = '|Create [J]ournal Entry' },
+            { '<leader>ni', '<cmd>Neorg index<cr>', description = '|Open [I]ndex Entry' },
           },
         },
       }
