0c7fc83034bea579cab0057b44d35971cd9cf3c9
happyori <orkhan.tahirov@gmail.com>
2025-05-20 13:10:48 -0700

    NixOS generation -[197]- 2025-05-20 Tue 01:10:47 PM

--- hosts/happypc/home.nix
+++ hosts/happypc/home.nix
@@ -88,7 +88,7 @@
         enable = false;
       }
       {
-        name = "DP-2";
+        name = "DP-1";
         width = 2560;
         height = 1440;
         refresh = 169.83;
@@ -97,7 +97,7 @@
         scale = "auto";
       }
       {
-        name = "DP-1";
+        name = "DP-2";
         width = 3440;
         height = 1440;
         refresh = 164.90;
