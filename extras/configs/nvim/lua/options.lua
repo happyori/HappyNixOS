219057de51d1fdfe83be5e5e23952ed8834b166d
happyori <orkhan.tahirov@gmail.com>
2025-07-17 14:31:40 -0700

    NixOS generation -[203]- 2025-07-17 Thu 02:31:39 PM

--- extras/configs/nvim/lua/options.lua
+++ extras/configs/nvim/lua/options.lua
@@ -56,7 +56,7 @@ op.formatexpr = "v:lua.require'conform'.formatexpr()"
 if vim.g.neovide then
   vim.g.neovide_floating_blur_amount_x = 2.0
   vim.g.neovide_floating_blur_amount_y = 2.0
-  vim.g.neovide_transparency = 0.8
+  vim.g.neovide_opacity = 0.8
   vim.g.neovide_refresh_rate = 144
   vim.g.neovide_cursor_smooth_blink = true
   vim.g.neovide_cursor_vfx_mode = 'railgun'
